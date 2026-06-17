ba20b1c614a2e578006bc2e099460fd1cd970833
Sam Mumford <smmmfrd@gmail.com>
2026-06-17 14:29:37 -0700

    Reading Data From File and Disable Writer

--- 1_read_file/readfile.go
+++ 1_read_file/readfile.go
@@ -1,7 +1,26 @@
 package readfile
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func ReadFile() {
-	fmt.Println("hello from read file!")
+	// f, err := os.OpenFile(, os.O_WRONLY|os.O_TRUNC, 0644)
+	// check(err)
+
+	// defer f.Close()
+
+	data, err := os.ReadFile("data/random.txt")
+	check(err)
+
+	var nums []int
+	for _, b := range data {
+		if b == 10 {
+			continue
+		}
+		nums = append(nums, int(b)-48)
+	}
+
+	fmt.Println(nums)
 }
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import readfile "github.com/smmmfrd/go-midterm/1_read_file"
 
 func main() {
-	readfile.WriteFile()
+	// readfile.WriteFile()
 
 	readfile.ReadFile()
 }
