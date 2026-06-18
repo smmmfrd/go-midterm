88fe303cbb8c8ffe89c6d3e0475111655332527b
Sam Mumford <smmmfrd@gmail.com>
2026-06-18 16:37:22 -0700

    First Fetch

--- 2_fetch/fetch.go
+++ 2_fetch/fetch.go
@@ -1,7 +1,27 @@
 package fetch
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func FetchURLs() {
-	fmt.Println("nothing better than goroutines!")
+	res, err := http.Get("http://www.google.com/robots.txt")
+	check(err)
+
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		fmt.Printf("Failed fetch with code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	check(err)
+
+	fmt.Printf("%s\n", body)
 }
