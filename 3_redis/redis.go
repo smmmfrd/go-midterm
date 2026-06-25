d88a4179652cba369866f13f12a7839a471ab442
Sam Mumford <smmmfrd@gmail.com>
2026-06-25 13:03:59 -0700

    Running Server and Sending Messages from same script

--- 3_redis/redis.go
+++ 3_redis/redis.go
@@ -1,7 +1,69 @@
 package redis
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
 
+// Runs some checks on the Redis
 func Run() {
-	fmt.Println("hello from redis!")
+	fmt.Println("hello from the test script")
+
+	conn, err := net.Dial("tcp", "localhost:8090")
+	if err != nil {
+		fmt.Println("Error connecting to tcp server: ", err)
+		return
+	}
+
+	defer conn.Close()
+
+	fmt.Fprintf(conn, "hello from run\n")
+
+	reader := bufio.NewReader(conn)
+	res, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading response: ", err)
+		return
+	}
+
+	fmt.Println("Response: ", res)
+}
+
+// Starts the Redis
+func Start() {
+	listener, err := net.Listen("tcp", "localhost:8090")
+	if err != nil {
+		fmt.Println("Error creating tcp server: ", err)
+		return
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Started TCP server at localhost:8090")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting conn: ", err)
+			continue
+		}
+
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading message: ", err)
+		return
+	}
+
+	fmt.Printf("Received message: %s\n", message)
+
+	conn.Write([]byte("Message Received\n"))
 }
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import redis "github.com/smmmfrd/go-midterm/3_redis"
+import (
+	"time"
+
+	redis "github.com/smmmfrd/go-midterm/3_redis"
+)
 
 func main() {
 	// readfile.WriteFile()
@@ -8,6 +12,10 @@ func main() {
 	// readfile.ReadFile()
 
 	// fetch.FetchURLs()
+
+	go redis.Start()
+
+	time.Sleep(10 * time.Millisecond)
 
 	redis.Run()
 }
