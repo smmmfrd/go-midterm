dc86942579032de4714e392920d394747d4b32f1
Sam Mumford <smmmfrd@gmail.com>
2026-07-23 19:39:57 -0700

    Reading From Files

--- 4_json_loader/json_loader.go
+++ 4_json_loader/json_loader.go
@@ -1,7 +1,27 @@
 package jsonloader
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func JsonLoader() {
-	fmt.Println("hello from json loader")
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+var files = []string{"data/json/good.json", "data/json/bad.json"}
+
+func ReadJson() {
+	for _, v := range files {
+		JsonLoader(v)
+	}
+}
+
+func JsonLoader(filename string) {
+	data, err := os.ReadFile(filename)
+	check(err)
+
+	fmt.Println(string(data))
 }
--- main.go
+++ main.go
@@ -15,5 +15,5 @@ func main() {
 
 	// redis.Client()
 
-	jsonloader.JsonLoader()
+	jsonloader.ReadJson()
 }
