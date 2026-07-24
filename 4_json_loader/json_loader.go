5eafed8086290131f1c6d90c87bb4fbcc1d4e737
Sam Mumford <smmmfrd@gmail.com>
2026-07-23 19:52:06 -0700

    Files into Struct

--- 4_json_loader/json_loader.go
+++ 4_json_loader/json_loader.go
@@ -1,6 +1,7 @@
 package jsonloader
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
@@ -11,7 +12,23 @@ func check(e error) {
 	}
 }
 
-var files = []string{"data/json/good.json", "data/json/bad.json"}
+type ServerJSON struct {
+	Server struct {
+		Host    string `json:"host"`
+		Port    int    `json:"port"`
+		Timeout int    `json:"timeout"`
+	} `json:"server"`
+	Database struct {
+		Url            string `json:"url"`
+		MaxConnections int    `json:"max_connections"`
+	} `json:"database"`
+	Logging struct {
+		Level string `json:"level"`
+		File  string `json:"file"`
+	} `json:"logging"`
+}
+
+var files = []string{"data/json/good.json", "data/json/bad.json", "data/json/wrong.json"}
 
 func ReadJson() {
 	for _, v := range files {
@@ -20,8 +37,15 @@ func ReadJson() {
 }
 
 func JsonLoader(filename string) {
-	data, err := os.ReadFile(filename)
+	filedata, err := os.ReadFile(filename)
 	check(err)
 
-	fmt.Println(string(data))
+	fmt.Println(string(filedata))
+
+	serverJSON := ServerJSON{}
+
+	err = json.Unmarshal(filedata, &serverJSON)
+	check(err)
+
+	fmt.Println(serverJSON)
 }
