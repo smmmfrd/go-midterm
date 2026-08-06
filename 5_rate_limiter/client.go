0b1f7a2c82721b734dd68fdeccc4023aff66381a
Sam Mumford <smmmfrd@gmail.com>
2026-08-06 13:18:55 -0700

    Client Sequentially Making Requests

--- 5_rate_limiter/client.go
+++ 5_rate_limiter/client.go
@@ -8,18 +8,28 @@ import (
 
 func BeginClient() {
 	fmt.Println("beginning spamming")
+
+	for i := range 10 {
+		err := makeReq()
+		if err != nil {
+			fmt.Printf("Client failed after %d requests", i+1)
+			break
+		}
+	}
+}
+
+func makeReq() error {
 	resp, err := http.Get("http://localhost:9009/")
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		return err
 	}
 	defer resp.Body.Close()
 
 	text, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		return err
 	}
 
 	fmt.Printf("Client received: %s\n", string(text))
+	return nil
 }
