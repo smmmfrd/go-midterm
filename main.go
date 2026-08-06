ddffd211598a2913d477426816bbf6e2e7900074
Sam Mumford <smmmfrd@gmail.com>
2026-08-06 13:43:20 -0700

    Running all Assignments

--- main.go
+++ main.go
@@ -1,29 +1,37 @@
 package main
 
 import (
+	"sync"
 	"time"
 
+	readfile "github.com/smmmfrd/go-midterm/1_read_file"
+	fetch "github.com/smmmfrd/go-midterm/2_fetch"
+	redis "github.com/smmmfrd/go-midterm/3_redis"
+	jsonloader "github.com/smmmfrd/go-midterm/4_json_loader"
 	ratelimiter "github.com/smmmfrd/go-midterm/5_rate_limiter"
 )
 
 func main() {
-	var serverDuration time.Duration = 10 * time.Second
-	// readfile.WriteFile()
+	var serverDuration time.Duration = 3 * time.Second
+	var wg sync.WaitGroup
+	readfile.WriteFile()
 
-	// readfile.ReadFile()
+	readfile.ReadFile()
 
-	// fetch.FetchURLs()
+	fetch.FetchURLs()
 
-	// go redis.Start()
+	wg.Add(1)
+	go redis.Start(serverDuration, &wg)
 
-	// time.Sleep(serverDuration * time.Second)
+	redis.Client()
 
-	// redis.Client()
+	wg.Wait()
 
-	// jsonloader.ReadJson()
+	jsonloader.ReadJson()
 
-	go ratelimiter.Start(serverDuration)
+	wg.Add(1)
+	go ratelimiter.Start(serverDuration, &wg)
 
 	ratelimiter.BeginClient()
-	time.Sleep(serverDuration)
+	wg.Wait()
 }
